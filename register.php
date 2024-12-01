45b37edd999f3fd958b5c8ab75c35f7894dcdd7b
nim1236 <147692828+nim1236@users.noreply.github.com>
2024-12-01 17:58:18 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
 session_start();
 require 'db.php'; // Подключаем файл с подключением к базе данных
 
